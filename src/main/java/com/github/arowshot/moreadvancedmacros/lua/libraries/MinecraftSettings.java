6d45c798eb8e716f74fae64f2c185dee49324b72
Kyle <Arow411@gmail.com>
2018-12-29 22:15:51 -0500

    Fixed building, bumped version, removed useless getMinecraft() call

--- src/main/java/com/github/arowshot/moreadvancedmacros/lua/libraries/MinecraftSettings.java
+++ src/main/java/com/github/arowshot/moreadvancedmacros/lua/libraries/MinecraftSettings.java
@@ -48,7 +48,7 @@ public class MinecraftSettings extends TwoArgFunction implements LuaPlugin {
 		library.set("getfps", new ZeroArgFunction() {
 	        @Override
 			public LuaValue call() {
-				return LuaValue.valueOf(Minecraft.getMinecraft().getDebugFPS());
+				return LuaValue.valueOf(Minecraft.getDebugFPS());
 			}
 		});
 		return library;
